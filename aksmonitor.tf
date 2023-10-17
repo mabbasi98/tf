43878a90fef803ff0120892b746adb21bf8f9980
M Abbasi <121091435+mabbasi98@users.noreply.github.com>
2023-10-17 23:49:37 +0500

    Update aksmonitor.tf

--- aksmonitor.tf
+++ aksmonitor.tf
@@ -1,3 +1,14 @@
+resource "azurerm_monitor_action_group" "main" {
+  name                = "test-ahmemo07-aks"
+  resource_group_name = "app-block-prod-eastus"
+  short_name          = "test-ahmed"
+  email_receiver {
+    name          = "send to ahmed"
+    email_address = "mohammed.ahmed1@lexisnexisrisk.com"
+
+  }
+}
+
 locals {
   nodes = keys(var.node_groups)
 }
@@ -5,12 +16,12 @@ locals {
 
 
 resource "azurerm_monitor_metric_alert" "alert_aks-node-cpu-percentage" {
-  name                = "AKS_Node_CPU_Percentage-${var.Purpose}"
-  resource_group_name = ["************"]
-  scopes              = ["************"]
-  description         = "${var.AKSClusterName} Node CPU Percentage"
+  name                = "AKS_Node_CPU_Percentage"
+  resource_group_name = "app-block-prod-eastus"
+  scopes              = [module.aks_cluster.cluster_id]
+  description         = "${var.cluster_name} Node CPU Percentage"
 
-  
+
   criteria {
     metric_namespace = "Insights.Container/nodes"
     metric_name      = "cpuUsagePercentage"
@@ -19,78 +30,78 @@ resource "azurerm_monitor_metric_alert" "alert_aks-node-cpu-percentage" {
     threshold        = "80"
 
     dynamic "dimension" {
-    for_each = [local.nodes[criteria.key]]
-    content {
+      for_each = [local.nodes[criteria.key]]
+      content {
         name     = "Name of Node"
         operator = "Include"
         values   = dimension.key
-    }
+      }
     }
   }
-   
+
   action {
-    action_group_id = ["************"]
+    action_group_id = azurerm_monitor_action_group.main.id
   }
- 
+
 }
 
 
 
 resource "azurerm_monitor_metric_alert" "node-memory-percentage" {
   name                = "AKS_Node_Memory_Percentage"
-  resource_group_name = ["************"]
-  scopes              = ["************"]
-  description         = "${var.AKSClusterName} Node Memory Percentage"
+  resource_group_name = "app-block-prod-eastus"
+  scopes              = [module.aks_cluster.cluster_id]
+  description         = "${var.cluster_name} Node Memory Percentage"
 
- 
+
   criteria {
     metric_namespace = "Insights.Container/nodes"
     metric_name      = "memoryWorkingSetPercentage"
     aggregation      = "Average"
     operator         = "GreaterThan"
     threshold        = "80"
- 
+
     dynamic "dimension" {
-    for_each = [local.nodes[criteria.key]]
-    content {
+      for_each = [local.nodes[criteria.key]]
+      content {
         name     = "Name of Node"
         operator = "Include"
         values   = dimension.key
-    }
+      }
     }
   }
-   
+
   action {
-    action_group_id = ["************"]
+    action_group_id = azurerm_monitor_action_group.main.id
   }
- 
+
 }
 resource "azurerm_monitor_metric_alert" "node-disk-percentage" {
-  name                = "AKS_Node_Memory_Percentage"
-  resource_group_name = ["************"]
-  scopes              = ["************"]
-  description         = "${var.AKSClusterName} Node Disk Percentage"
+  name                = "AKS_Node_Disk_Percentage"
+  resource_group_name = "app-block-prod-eastus"
+  scopes              = [module.aks_cluster.cluster_id]
+  description         = "${var.cluster_name} Node Disk Percentage"
 
- 
+
   criteria {
     metric_namespace = "Insights.Container/nodes"
     metric_name      = "DiskUsedPercentage"
     aggregation      = "Average"
     operator         = "GreaterThan"
     threshold        = "80"
- 
+
     dynamic "dimension" {
-    for_each = [local.nodes[criteria.key]]
-    content {
+      for_each = [local.nodes[criteria.key]]
+      content {
         name     = "Name of Node"
         operator = "Include"
         values   = dimension.key
-    }
+      }
     }
   }
-   
+
   action {
-    action_group_id = ["************"]
+    action_group_id = azurerm_monitor_action_group.main.id
   }
 
 }
